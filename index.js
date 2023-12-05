4d6eabfed2342d06cde1f99c829ec33e6833fe49
AzimjonDev <azimjonqoshoqov@gmail.com>
2023-12-05 15:27:57 +0500

    number

---
+++ index.js
@@ -0,0 +1,49 @@
+// let a = 20;
+// let b = a + a + 40 + "20" + (a + a) + String(a) // 80204020
+
+// console.log(a);
+// console.log(b);
+
+// let a = 30;
+// let b = Number("20")
+
+// let x=20;
+// let y=30;
+// let result=y+x
+
+
+
+// console.log(result);
+
+
+// let x=20;
+// let y=30;
+// let result=y-x;
+// console.log(result);
+
+
+
+// let x=20;
+// let y=30;
+// let result=y*x;
+// console.log(result);
+
+
+// let x=20;
+// let y=30;
+// let result=y/x;
+// console.log(resul
+
+// let y=30;
+// let result=Math.pow(y)
+// console.log(result);
+
+
+
+function sum(a,b,c) {
+  return  a+b*c
+}
+
+
+
+console.log( sum(10,20,30));
