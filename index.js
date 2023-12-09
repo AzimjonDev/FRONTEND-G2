b30907de9714608b71a515ee44085e2cd78d56a0
AzimjonDev <azimjonqoshoqov@gmail.com>
2023-12-09 15:12:46 +0500

    finish code

--- index.js
+++ index.js
@@ -1,55 +1,77 @@
-
-// function res(a,b,c,d) {
-//   return  ` bug'da ${a+d} bor va  ${b+d} ham bor`
-// }
-
-// console.log( res("gul","lola","bola","lar"));
-
-
-// function monkeyTrouble(aSmile, bSmile){
-//   return aSmile==bSmile
-// }
-
-// console.log(monkeyTrouble(true, true))// → true
-// console.log(monkeyTrouble(false, false)) //→ true
-// console.log(monkeyTrouble(true, false))// → false
-
-
-// if else //
-// function  res(enter,age1,age2,age3) {
-//   if( age2 == age1 && age3 ==enter || enter==age2){
-//     console.log(true);
-//   }else{
-//     console.log(false);
+// switch ("yakshanba") {
+// 	case "yakshanba":
+// 	 console.log( day = "Sunday" );
+// 	  break;
+// 	case 1:
+// 	 console.log( day = "Monday" );
+// 	  break;
+// 	case 2:
+// 	  console.log( day = "Tuesday ");
+// 	  break;
+// 	case 3:
+// 	 console.log( day = "Wednesday ");
+// 	  break;
+// 	case 4:
+// 	 console.log( day = "Thursday");
+// 	  break;
+// 	case 5:
+// 	 console.log( day = "Friday" );
+// 	  break;
+// 	case 6:
+// 	 console.log( day = "Saturday");
 //   }
-// }
-// res(30,30,30,400)
 
-
-
-// function  res(enter ,age1) {
-//   if( enter <= age1 ){
-//     console.log("Ruxsat");
-//   }else{
-//     console.log("Ruxsat yo'q");
+//   switch (prompt()) {
+// 	case "one":
+// 		console.log(alert("bir"));
+// 		break;
+// 		case "two":
+// 			console.log(alert("ikki"));
+// 			break;
+// 			default:console.log(alert("bizda bunday malumot yoq"));
+// 		break;
 //   }
+
+
+
+// function foo(d) {
+// 	console.log(a);
+//    var a=20;
+//    let b=10;
+//    const c=30;
 // }
-// res(18,18)
-// res(20,19)
-// res(50,31)
+
+// foo();
+
+// function show() {
+// 	console.log("arslonbek");
+// }
+// let app = show();
+// console.log(app);
 
 
+// let a = 20;
+// function app(){
+// 	console.log(a);
+// 	console.log("hello world");
+// }
+// app()
 
-// =
-// ==
-// ===
-// &&
-// ||
-function sum(params) {
-  let a=20;
-	console.log(b);
-	let b=30;
-	var d=40
+app()
+function app() {
+	   console.log("Arslonbek");
 }
-console.log(d);
+
+var show = function (a, b) {
+	console.log("hello world");
+};
+show()
+
+const sum=()=>{
+	console.log("Jamshidbek");
+}
 sum()
+
+
+const summa=()=>console.log("Salom");
+summa()
