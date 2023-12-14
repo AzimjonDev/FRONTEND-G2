b023c30dd133b84101ae38c14145c45ed1f95bef
AzimjonDev <azimjonqoshoqov@gmail.com>
2023-12-14 15:27:45 +0500

    codingJs

--- index.js
+++ index.js
@@ -1,15 +1,81 @@
 
-let a=-2;
-let b=3;
-let c=-4;
+// let a=2;
+// let b=-3;
+// let c=4;
+// let negative=0;
+// let positive=0
+
+// if(a<0 ){
+//     negative++//0
+// }else{
+//     positive++//1
+// } 
+//  if (b<0){
+//     negative++//1
+// } else{
+//     positive++//1
+// }
+// if(c<0){
+//     negative++//1
+// }else{
+//     positive++//1+1=2
+// }
+// if(negative>0){
+//     console.log(negative);//1
+// }if(positive>0){
+//     console.log(positive);//2
+// }
 
 
-if(a<0 ){
-    console.log(a);
-} if (b<0){
-    console.log(b);
-}if(c<0){
-    console.log(c);
-}else{
-    console.log("hechqanday manfiy son mavjud emas");
-}
+
+
+
+// let a=prompt("brinchi sonni kriting");
+// let b=prompt("ikkinchi sonni kriting");
+// if( a<b){
+//   console.log("kichiksonni tartib raqami 1-son" );
+// }else {
+//     console.log("kichiksonni tartib raqami 2-son");
+// }
+
+// let a=3;
+// let b=4;
+// let max=0
+// let min=0
+
+// if(a>b){
+// max=a
+// min=b
+// } if(b>a){
+//     max=b;
+//     min=a
+// }
+// console.log(max,min);
+
+// let a=5;
+// let b=8;
+
+// let max=0
+// let min=0;
+// if(a<b){
+//     min=b//8
+// } if(a<b){
+// max=a
+// }
+// console.log(min,max);
+
+// let a=10;
+// let b=10;
+// let sum=0
+
+
+// if(a!==b){
+//     sum=a+b
+//     a=sum
+//     b=sum
+// console.log(a,b);
+
+// }else{
+//     console.log(0);
+// }
+
