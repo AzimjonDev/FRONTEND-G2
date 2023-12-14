02cf72b4a23d4713249d4b5da1ad080839a32746
AzimjonDev <azimjonqoshoqov@gmail.com>
2023-12-14 12:07:42 +0500

    first code

--- index.js
+++ index.js
@@ -1,77 +1,15 @@
-// switch ("yakshanba") {
-// 	case "yakshanba":
-// 	 console.log( day = "Sunday" );
-// 	  break;
-// 	case 1:
-// 	 console.log( day = "Monday" );
-// 	  break;
-// 	case 2:
-// 	  console.log( day = "Tuesday ");
-// 	  break;
-// 	case 3:
-// 	 console.log( day = "Wednesday ");
-// 	  break;
-// 	case 4:
-// 	 console.log( day = "Thursday");
-// 	  break;
-// 	case 5:
-// 	 console.log( day = "Friday" );
-// 	  break;
-// 	case 6:
-// 	 console.log( day = "Saturday");
-//   }
 
-//   switch (prompt()) {
-// 	case "one":
-// 		console.log(alert("bir"));
-// 		break;
-// 		case "two":
-// 			console.log(alert("ikki"));
-// 			break;
-// 			default:console.log(alert("bizda bunday malumot yoq"));
-// 		break;
-//   }
+let a=-2;
+let b=3;
+let c=-4;!~
 
 
-
-// function foo(d) {
-// 	console.log(a);
-//    var a=20;
-//    let b=10;
-//    const c=30;
-// }
-
-// foo();
-
-// function show() {
-// 	console.log("arslonbek");
-// }
-// let app = show();
-// console.log(app);
-
-
-// let a = 20;
-// function app(){
-// 	console.log(a);
-// 	console.log("hello world");
-// }
-// app()
-
-app()
-function app() {
-	   console.log("Arslonbek");
+if(a<0 ){
+    console.log(a);
+} if (b<0){
+    console.log(b);
+}if(c<0){
+    console.log(c);
+}else{
+    console.log("hechqanday manfiy son mavjud emas");
 }
-
-var show = function (a, b) {
-	console.log("hello world");
-};
-show()
-
-const sum=()=>{
-	console.log("Jamshidbek");
-}
-sum()
-
-
-const summa=()=>console.log("Salom");
-summa()
