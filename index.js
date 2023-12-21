da5d3edf82b0154e6533c271f6131594e66d160d
AzimjonDev <azimjonqoshoqov@gmail.com>
2023-12-21 13:33:12 +0500

    Strin methods finish code

--- index.js
+++ index.js
@@ -1,81 +1,59 @@
+// let str="uzbekistan"
 
-// let a=2;
-// let b=-3;
-// let c=4;
-// let negative=0;
-// let positive=0
+// console.log(str.replace(str,"O'ZBEKISTON"));
 
-// if(a<0 ){
-//     negative++//0
-// }else{
-//     positive++//1
-// } 
-//  if (b<0){
-//     negative++//1
-// } else{
-//     positive++//1
+// let str ="s.a.l.o.m"
+// console.log(str.replaceAll("."," "));
+
+//      //  0123456789
+// let str="uzbekistan"
+// console.log(str.charAt(5));
+
+
+// let str="uzbekistan";
+// console.log(str.length);
+
+// let str="uzbekistan"
+// console.log(str.includes("l"));
+
+
+// let str="uzbekistan"
+// console.log(str.indexOf("n"));
+
+
+// let str="uzbekistan"
+// console.log(str.lastIndexOf("u"));
+
+// let str="uzbekistan "
+// console.log(str.repeat());
+
+
+// let str="uzbekistan"
+// console.log(str.slice());
+
+
+// let str="uzbekistan"
+// console.log(str.split([]));
+
+
+
+// let str="uzbekistan"
+// console.log(str.substring(1,9));
+
+
+
+// let str=" uzbekistan  "
+// console.log(str.length);
+// console.log(str.trim().length);
+
+
+// function parrotTrouble(talking, hour){
+//      return talking&&(hour<7||hour>20)
 // }
-// if(c<0){
-//     negative++//1
-// }else{
-//     positive++//1+1=2
-// }
-// if(negative>0){
-//     console.log(negative);//1
-// }if(positive>0){
-//     console.log(positive);//2
-// }
+
+// console.log(parrotTrouble(true, 6)); //→ true
+// console.log(parrotTrouble(true, 7)); //→ false
+// console.log(parrotTrouble(false, 6) );//→ false
 
 
-
-
-
-// let a=prompt("brinchi sonni kriting");
-// let b=prompt("ikkinchi sonni kriting");
-// if( a<b){
-//   console.log("kichiksonni tartib raqami 1-son" );
-// }else {
-//     console.log("kichiksonni tartib raqami 2-son");
-// }
-
-// let a=3;
-// let b=4;
-// let max=0
-// let min=0
-
-// if(a>b){
-// max=a
-// min=b
-// } if(b>a){
-//     max=b;
-//     min=a
-// }
-// console.log(max,min);
-
-// let a=5;
-// let b=8;
-
-// let max=0
-// let min=0;
-// if(a<b){
-//     min=b//8
-// } if(a<b){
-// max=a
-// }
-// console.log(min,max);
-
-// let a=10;
-// let b=10;
-// let sum=0
-
-
-// if(a!==b){
-//     sum=a+b
-//     a=sum
-//     b=sum
-// console.log(a,b);
-
-// }else{
-//     console.log(0);
-// }
 
