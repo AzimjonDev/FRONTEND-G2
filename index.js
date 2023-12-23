239a5090a2e7c24d5f3c7389fd4ba07d35632a2b
AzimjonDev <azimjonqoshoqov@gmail.com>
2023-12-23 15:20:17 +0500

    array methods map filter fil sort

--- index.js
+++ index.js
@@ -7,17 +7,47 @@
 
 // array//massive
 
-let oquvchi=["Boburbek","Arslonbek","Jamshidbek",1,4,true,false,null ,12]
+// let oquvchi=["Boburbek","Arslonbek","Jamshidbek",1,4,true,false,null ,12]
 
-console.log(oquvchi[oquvchi.length-1]);
+// console.log(oquvchi[oquvchi.length-1]);
 
 // let str=" Salom"
 // console.log(oquvchi[0].concat(str));
 
 
-let str=" Salom"
-oquvchi.push(str);
-oquvchi.pop(str)
-oquvchi.unshift(str)
-oquvchi.shift();
-console.log(oquvchi);
+// let str=" Salom"
+// oquvchi.push(str);
+// oquvchi.pop(str)
+// oquvchi.unshift(str)
+// oquvchi.shift();
+// console.log(oquvchi);
+
+
+
+
+// const array1 = [1, 4, 9, 16];
+// const map1 = array1.map((x) => x * 2);
+// console.log(map1);
+
+// const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const result = words.filter((word) => word.length >= 6);
+
+// console.log(result);
+
+
+// const months = ['March', 'Jan', 'Feb', 'Dec'];
+// months.sort();
+// console.log(months);
+// // Expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+// const array1 = [1, 3, 4, 2, 1];
+// array1.sort();
+// console.log(array1);
+
+const array1 = [1, 2, 3, 4];
+ console.log(  array1)
+
+
+
+
