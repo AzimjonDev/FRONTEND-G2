6a32417446a7a1a505d5321860e58acbc889675e
AzimjonDev <azimjonqoshoqov@gmail.com>
2024-01-18 14:14:43 +0500

    finish code while

--- index.js
+++ index.js
@@ -1,47 +1,20 @@
+let str="uzbekistan"
 
 
 
-// const nums=[1,2,3,4,5,]
-//     nums.reverse();
-// console.log(nums);
 
-// let sum= nums.reduce((a,c)=>{
-//    return a+c
-// })
-// console.log(sum)
 
-//  nums.forEach((item,idx)=>{
-// console.log(item+2,);
-//  })
 
-// const nums=[1,2,3,4,5,6]
-// const sum=[1,2,23,44,4]
-// const res=nums.reduce((p,c)=>{
-//   return p + c
-// }
-
-// )
-// console.log(res);
-// const res=nums.concat(sum).reverse()
-// console.log(nums);
-// console.log(res);
-// let ward=['salom dunyo',"guli"]
-
-// ward.reverse();
-// console.log(ward);
-
-const str='salom dunyo'
-const res=str
-.split('')
-.reverse()
-.join(' ');
+function customToUpperCase(str) {
  
-console.log(res);
+let res=""
+for(let i=0;i<str.length;i++){
+  res+=str[i].toUpperCase()
+}
+    return res;
+}
 
-
-
-
-
+ console.log(customToUpperCase("Salom, dunyo!"));
 
 
 
