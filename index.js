0793b5581f64d8c607f3201b8ca4f1e7c54f15b4
AzimjonDev <azimjonqoshoqov@gmail.com>
2024-02-13 15:16:16 +0500

    finish code

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-let text=document.querySelector(".text")
-let input=document.querySelector("#id")
-let btn=document.querySelector(".btn")
-console.log(btn);
-
-btn.addEventListener("click",function () {
-    const title=input.value
-    text.innerHTML=title
-})
+let item=document.querySelector(".item")
+let head=document.querySelector(".head")
 
 
+// let box=item.getAttribute("href")
+
+// item.setAttribute("href","https://uzum.uz/ru")
+
+// /** createElement */
+// Creating a button element
+const element = document.createElement("button");
+element.innerHTML = "I Am Button";
+
+
+document.body.appendChild(element);
+
+const h1 = document.createElement("h1");
+h1.innerHTML = "Salom";
+
+
+document.body.button.appendChild(h1);
+console.log(h1);
 
 
 
